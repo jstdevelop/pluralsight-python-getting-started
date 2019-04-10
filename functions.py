b049b052a9de6f6dc1e4f1fb241c26c107fcbd8a
Joseph Turner <joseph.turner@tcboe.org>
2019-04-10 12:13:51 -0500

    end of functions, files, yield,and lambda

--- functions.py
+++ functions.py
@@ -3,7 +3,7 @@ students = []
 def get_students_titlecase():
     students_titlecase = []
     for student in students:
-        students_titlecase = student["name"].title()
+        students_titlecase.append(student["name"].title())
     return students_titlecase
 
 def print_students_titlecase():
@@ -31,11 +31,11 @@ def read_file():
     except Exception:
         print("Could not read file")
 
-
-student_list = get_students_titlecase()
+read_file()
+print_students_titlecase()
 
 student_name = input("Enter student name: ")
 student_id = input("Enter student ID: ")
 
 add_student(student_name, student_id)
-print_students_titlecase()
+save_file(student_name)
